9f793d87db7a5718d2ceb34be4c4b0f1874cd8bb
spoonm <spoonm@spoonm.org>
2015-09-07 23:12:49 -0300

    Retirada definição de struct desnecessária.

--- tela.h
+++ tela.h
@@ -1,13 +1,6 @@
 #ifndef TELA_H
 #define TELA_H
 
-typedef struct
-{
-    char** cont;
-    int larg;
-    int alt;
-} Tela;
-
 void mostrar_tela (char tela[25][50]);
 void criar_tela (char tela[25][50]);
 
