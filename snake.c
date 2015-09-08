3f9620dcad82bb64a899bf9185e22fba327d5a1a
spoonm <spoonm@spoonm.org>
2015-09-07 22:21:29 -0300

    Removido chaves redundantes.

--- snake.c
+++ snake.c
@@ -18,28 +18,16 @@ void mostrarMatrix(char matrix[25][50]) {
 void criarMatrix(char matrix[25][50]) {
 	int i, k;
 	for ( i = 0; i < 25; ++i)
-	{
 		for ( k = 0; k < 50; ++k)
-		{
 			matrix[i][k] = ' ';
-		}
-	}
 	for ( i = 0; i < 50; ++i)
-	{
 		matrix[0][i] = '#';
-	}
 	for ( i = 0; i < 25; ++i)
-	{
 		matrix[i][0] = '#';
-	}
 	for ( i = 0; i < 25; ++i)
-	{
 		matrix[i][49] = '#';
-	}
 	for ( i = 0; i < 50; ++i)
-	{
 		matrix[24][i] = '#';
-	}
 }
 
 int main(int argc, char const *argv[])
