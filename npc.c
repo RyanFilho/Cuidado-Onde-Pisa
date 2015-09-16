c3b08683db7074d2b09b815e3234c2fb3638898b
spoonm <spoonm@spoonm.org>
2015-09-16 14:59:30 -0300

    Posição será dada para essa função.

--- npc.c
+++ npc.c
@@ -4,9 +4,7 @@
 
 void criar_cobra (char tela[25][50], int posicao[2])
 {
-	tela[12][25] = '@';
-	posicao[0] = 12;
-	posicao[1] = 25;
+	tela[posicao[0]][posicao[1]] = '@';
 }
 
 void mover_baixo (char tela[25][50], int posicao[2])
