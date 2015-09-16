b2c4ca8bd0d01b9a1ea6dba445a12240c4d011b6
spoonm <spoonm@spoonm.org>
2015-09-16 14:28:38 -0300

    Tipo de retorno de verificar_morte mudado para void.

--- npc.c
+++ npc.c
@@ -2,36 +2,44 @@
 #include <stdlib.h>
 #include "npc.h"
 
-void criar_cobra(char tela[25][50], int posicao[2]){
+void criar_cobra (char tela[25][50], int posicao[2])
+{
 	tela[12][25] = '@';
 	posicao[0] = 12;
 	posicao[1] = 25;
 }
 
-void mover_baixo(char tela[25][50], int posicao[2]){
+void mover_baixo (char tela[25][50], int posicao[2])
+{
 	tela[posicao[0]][posicao[1]] = ' ';
 	posicao[0]++;
 	tela[posicao[0]][posicao[1]] = '@';
 }
-void mover_cima(char tela[25][50], int posicao[2]){
+void mover_cima (char tela[25][50], int posicao[2])
+{
 	tela[posicao[0]][posicao[1]] = ' ';
 	posicao[0]--;
 	tela[posicao[0]][posicao[1]] = '@';
 }
-void mover_esquerda(char tela[25][50], int posicao[2]){
+void mover_esquerda (char tela[25][50], int posicao[2])
+{
 	tela[posicao[0]][posicao[1]] = ' ';
 	posicao[1]--;
 	tela[posicao[0]][posicao[1]] = '@';
 }
-void mover_direita(char tela[25][50], int posicao[2]){
+void mover_direita (char tela[25][50], int posicao[2])
+{
 	tela[posicao[0]][posicao[1]] = ' ';
 	posicao[1]++;
 	tela[posicao[0]][posicao[1]] = '@';
 }
 
-int verificar_morte(int posicao[2]){
-	if (posicao[0] > 23 || posicao[1] > 48 || posicao[0] < 1 || posicao[1] < 1 )
-	{
+void verificar_morte(int posicao[2])
+{
+	if (
+        posicao[0] > 23 || posicao[1] > 48 ||
+        posicao[0] < 1  || posicao[1] < 1 
+    ) {
 		system("cls");
 		printf("\n\n\n\n\n\n\n                                Voce morreu !\n");
 		exit(1);
