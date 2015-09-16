888b675a3c3b4c435ce6f410c49e55877df1fedc
spoonm <spoonm@spoonm.org>
2015-09-16 15:00:07 -0300

    Tabs devem ser espaços. Adicionado default. Constantes corrigidas.

--- snake.c
+++ snake.c
@@ -4,44 +4,50 @@
 #include "npc.h"
 #include "tela.h"
 
-#define CIMA 75
+#define CIMA 72
 #define BAIXO 80
 #define DIREITA 77
-#define ESQUERDA -32
+#define ESQUERDA 75
 
 int main ()
 {
-	char tela[25][50];
-	char direcao = 77;
-	int posicao[2];
-	criar_tela(tela);
-	criar_cobra(tela , posicao);
+    char tela[25][50];
+    char direcao = 77;
+    int posicao[2] = {3, 3};
+    criar_tela(tela);
+    criar_cobra(tela , posicao);
 
-	while(1){
-		system("cls");
-		mostrar_tela(tela);
+    while (1) {
+        system("cls");
+        mostrar_tela(tela);
 
 
-		while(kbhit()){
-			direcao = getch();
-		}
-		switch(direcao){
-			case CIMA:
+        while (kbhit())
+            direcao = getch();
+            
+        
+        switch (direcao)
+        {
+            case CIMA:
                 mover_cima(tela, posicao);
                 break;
-			case BAIXO:
+            case BAIXO:
                 mover_baixo(tela, posicao);
                 break;
-			case ESQUERDA:
+            case ESQUERDA:
                 mover_esquerda(tela, posicao);
                 break;
-			case DIREITA:
+            case DIREITA:
                 mover_direita(tela, posicao);
                 break;
-		}
-		verificar_morte(posicao);
-		Sleep(100);
-	}
+            default:
+                // O que eu faço se o usuário apertou, digamos, A?
+                ;
+        }
+        
+        verificar_morte(posicao);
+        Sleep(100);
+    }
 
-	return 0;
+    return 0;
 }
