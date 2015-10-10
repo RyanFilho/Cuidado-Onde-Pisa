4a99b440266ad3f482f73b5e3d2f66cc5a9ca62e
spoonm <spoonm@spoonm.org>
2015-10-10 18:59:04 -0300

    Atualização do arquivo de cabeçalho.

--- npc.h
+++ npc.h
@@ -1,11 +1,8 @@
 #ifndef NPC_H
 #define NPC_H
 
-void mover_baixo        (display *tela, int posicao[2]);
-void mover_cima         (display *tela, int posicao[2]);
-void mover_direita      (display *tela, int posicao[2]);
-void mover_esquerda     (display *tela, int posicao[2]);
-
+void placar				(display *tela);
+void menu				(display *tela);
 void verificar_morte    (display *tela, int posicao[2]);
 void criar_inimigo      (display *tela, int inimigos[][2], int *n_inimigos);
 
