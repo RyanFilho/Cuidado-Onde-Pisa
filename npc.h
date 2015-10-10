28284c456412b05d655aae3a3d79cb2cda887eed
spoonm <spoonm@spoonm.org>
2015-10-10 19:07:06 -0300

    Conserto no arquivo de cabeçalho.

--- npc.h
+++ npc.h
@@ -1,7 +1,7 @@
 #ifndef NPC_H
 #define NPC_H
 
-void placar				(display *tela);
+void placar				(display *tela, int n_inimigos);
 void menu				(display *tela);
 void verificar_morte    (display *tela, int posicao[2]);
 void criar_inimigo      (display *tela, int inimigos[][2], int *n_inimigos);
